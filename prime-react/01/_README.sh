260d3723263fc81f39e710869b067c139f230985
ronald.pineda <rpineda@illuminateed.net>
2022-06-12 12:23:51 -0600

    02/init app

--- prime-react/01/_README.sh
+++ prime-react/01/_README.sh
@@ -1,8 +1,7 @@
 ##############################################################################
 # Source Tutorial: https://www.youtube.com/watch?v=Prz3phy2bHY
 ##############################################################################
-
-npx create-react-app prime-app
-cd prime-app; npm start;
+cd $RESEARCH_HOME/reactjs/prime-react/01
+npx create-react-app prime-app; cd prime-app; npm start;
 npm install primereact primeicons --save
 
---
+++ prime-react/02/_README.sh
@@ -0,0 +1,9 @@
+##############################################################################
+# Source Tutorial: https://www.youtube.com/watch?v=sxylafSufqc
+##############################################################################
+cd $RESEARCH_HOME/reactjs/prime-react/02
+npx create-react-app prime-app; cd prime-app; npm start;
+npm install primereact primeicons --save
+
+~/Library/CloudStorage/OneDrive-IntertecConsulting,LLC/_RESEARCH/reactjs/prime-react
+/Users/rpineda/Library/CloudStorage/OneDrive-IntertecConsulting,LLC/_RESEARCH/reactjs
