ca2d3bc4455de44755cd728b28260829dc764154
ronald.pineda <rpineda@illuminateed.net>
2022-06-12 12:15:14 -0600

    complete first tutorial

--- prime-react/XX/_README.sh
+++ prime-react/XX/_README.sh
@@ -2,7 +2,4 @@
 # Source Tutorial: https://www.youtube.com/watch?v=Prz3phy2bHY
 ##############################################################################
 
-npx create-react-app prime-app
-cd prime-app; npm start;
-npm install primereact primeicons --save
 
