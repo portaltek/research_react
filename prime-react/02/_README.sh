29e549bf9e62396ecfb0d7cb18c728f37b001279
ronald.pineda <rpineda@illuminateed.net>
2022-06-13 09:40:58 -0600

    init prime-app2

--- prime-react/02/_README.sh
+++ prime-react/02/_README.sh
@@ -3,6 +3,14 @@
 ##############################################################################
 cd $RESEARCH_HOME/reactjs/prime-react/02
 npx create-react-app prime-app; cd prime-app; npm start;
+npx create-react-app prime-app2; cd prime-app2; npm start;
 npm install primereact primeicons --save
 npm install classnames react-transition-group axios
 
+Components
+- MenuBar
+- MenuBarElement
+- SideBar
+- SideBarElement
+- CreateItemPage
+---
