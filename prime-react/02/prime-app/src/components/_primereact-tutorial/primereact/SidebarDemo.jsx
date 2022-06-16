77afa3186f0f13b7c74742aa8a27feec82487e32
ronald.pineda <rpineda@illuminateed.net>
2022-06-15 19:41:05 -0600

    primereact SidebarDemo

--- prime-react/02/prime-app/src/components/_primereact-tutorial/primereact/SidebarDemo.jsx
+++ prime-react/02/prime-app/src/components/_primereact-tutorial/primereact/SidebarDemo.jsx
@@ -5,18 +5,6 @@ import {Button} from 'primereact/button';
 export const SidebarDemo = () => {
     const [visibleLeft, setVisibleLeft] = useState(true);
 
-
-    const customIcons = (
-        <React.Fragment>
-            <button className="p-sidebar-icon p-link mr-1">
-                <span className="pi pi-print"/>
-            </button>
-            <button className="p-sidebar-icon p-link mr-1">
-                <span className="pi pi-arrow-right"/>
-            </button>
-        </React.Fragment>
-    );
-
     return (
         <div>
             <div className="card">
