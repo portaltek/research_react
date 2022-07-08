48e63060a2c3f8f41eb2ecd133731411cbc78b1d
ronald.pineda <>
2022-07-07 21:45:47 -0600

    Test003/Adding  custom table4

--- Test003/my-react-app/src/pages/product/Product.jsx
+++ Test003/my-react-app/src/pages/product/Product.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { ProductTable } from "./ProductTable";
 import { ProductTable2 } from "./ProductTable2";
 import { ProductTable3 } from "./ProductTable3";
+import EnhancedTable from "./ProductTable4";
 
 export const Product = () => {
   return (
     <>
-      <div>Product</div>
+      <div></div>
+      <EnhancedTable />
       {/* <ProductTable3 /> */}
       <ProductTable2 />
       {/* <ProductTable /> */}
