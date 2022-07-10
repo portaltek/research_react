81dfb6dba82193d5a3a8ca3288879b3059a6a802
ronald.pineda <>
2022-07-10 12:06:02 -0600

    Test004/sidemenu baseline

--- Test004/_README.sh
+++ Test004/_README.sh
@@ -7,10 +7,15 @@
 export APP_NAME=my-react-app
 export PROJECT_DIR=$RESEARCH_HOME/reactjs/Test004
 cd "$PROJECT_DIR"
-npx create-react-app $APP_NAME; cd $APP_NAME; npm start;
+npx create-react-app $APP_NAME; cd $PROJECT_DIR/$APP_NAME; npm start;
 
-# STOP PROJECT, DOWNLOAD DEPENDENCIES AND FILE CLEAN UP
+# IN ANOTHER TERMINAL, DOWNLOAD DEPENDENCIES AND FILE CLEAN UP
+export APP_NAME=my-react-app
+export PROJECT_DIR=$RESEARCH_HOME/reactjs/Test004
+cd  $PROJECT_DIR/$APP_NAME
 npm i @mui/material @emotion/react @emotion/styled @mui/icons-material react-router-dom
-mkdir src/pages; rm src/logo.svg public/logo192.png public/logo512.png
+mkdir src/pages src/_utils
+mkdir src/layout;
+rm src/logo.svg public/logo192.png public/logo512.png
 rm src/App.js
-echo "function App() {  return <div>APP</div>; } export default App;" >  src/App.js
+echo "function App() {  return <>APP</>; } export default App;" >  src/App.js
--- Test004/my-react-app/src/App.js
+++ Test004/my-react-app/src/App.js
@@ -1,1 +1,14 @@
-function App() {  return <div>APP</div>; } export default App;
+import Navbar from "./layout/navbar/Navbar";
+import { Box, CssBaseline } from "@mui/material";
+import Sidemenu from "./layout/sidemenu/Sidemenu";
+
+function App() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Navbar />
+      <CssBaseline />
+      <Sidemenu />
+    </Box>
+  );
+}
+export default App;
