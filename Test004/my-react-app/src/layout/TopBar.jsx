a366e25245544f5129861435fc9009759ee5f220
ronald.pineda <>
2022-07-10 17:07:26 -0600

    Test004/ Refactor Topbar

--- Test004/my-react-app/src/layout/TopBar.jsx
+++ Test004/my-react-app/src/layout/TopBar.jsx
@@ -1,60 +1,18 @@
 import AccountCircle from "@mui/icons-material/AccountCircle";
 import MailIcon from "@mui/icons-material/Mail";
 import MenuIcon from "@mui/icons-material/Menu";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import SchoolIcon from "@mui/icons-material/School";
 import AppBar from "@mui/material/AppBar";
 import Badge from "@mui/material/Badge";
+import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import InputBase from "@mui/material/InputBase";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
-import { styled, alpha } from "@mui/material/styles";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
-import Box from "@mui/material/Box";
-
-const Search = styled("div")(({ theme }) => ({
-  position: "relative",
-  borderRadius: theme.shape.borderRadius,
-  backgroundColor: alpha(theme.palette.common.white, 0.15),
-  "&:hover": {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-  },
-  marginRight: theme.spacing(2),
-  marginLeft: 0,
-  width: "100%",
-  [theme.breakpoints.up("sm")]: {
-    marginLeft: theme.spacing(3),
-    width: "auto",
-  },
-}));
-
-const SearchIconWrapper = styled("div")(({ theme }) => ({
-  padding: theme.spacing(0, 2),
-  height: "100%",
-  position: "absolute",
-  pointerEvents: "none",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: "inherit",
-  "& .MuiInputBase-input": {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("md")]: {
-      width: "20ch",
-    },
-  },
-}));
 
 export const TopBar = ({ handleMobileDrawerToggle, handleDrawerToggle }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
@@ -80,15 +38,87 @@ export const TopBar = ({ handleMobileDrawerToggle, handleDrawerToggle }) => {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
+  return (
+    <>
+      <AppBar
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+      >
+        <Toolbar variant="dense">
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={handleMobileDrawerToggle}
+            sx={{ mr: 2, display: { xs: "block", sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { xs: "none", sm: "block" } }}
+          >
+            <SchoolIcon />
+          </IconButton>
+
+          <Typography variant="h6" noWrap component="div">
+            MUI
+          </Typography>
+          <TopBarProfile {...{ handleProfileMenuOpen, handleMobileMenuOpen }} />
+        </Toolbar>
+      </AppBar>
+      <TopBarProfileMenu {...{ anchorEl, isMenuOpen, handleMenuClose }} />
+      <TobBarProfileMenuMobile
+        {...{
+          mobileMoreAnchorEl,
+          isMobileMenuOpen,
+          handleMobileMenuClose,
+          handleProfileMenuOpen,
+        }}
+      />
+    </>
+  );
+};
+
+const TopBarProfile = ({ handleProfileMenuOpen, handleMobileMenuOpen }) => {
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }} />
+      <Box sx={{ display: { xs: "none", md: "flex" } }}>
+        <IconButton size="large" color="inherit">
+          <Badge badgeContent={42} color="error">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton
+          size="large"
+          edge="end"
+          onClick={handleProfileMenuOpen}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
+      </Box>
+      <Box sx={{ display: { xs: "flex", md: "none" } }}>
+        <IconButton size="large" onClick={handleMobileMenuOpen} color="inherit">
+          <MoreVertIcon />
+        </IconButton>
+      </Box>
+    </>
+  );
+};
+
+const TopBarProfileMenu = ({ anchorEl, isMenuOpen, handleMenuClose }) => {
+  return (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
         vertical: "top",
         horizontal: "right",
       }}
-      id={menuId}
       keepMounted
       transformOrigin={{
         vertical: "top",
@@ -101,16 +131,21 @@ export const TopBar = ({ handleMobileDrawerToggle, handleDrawerToggle }) => {
       <MenuItem onClick={handleMenuClose}>My account</MenuItem>
     </Menu>
   );
+};
 
-  const mobileMenuId = "primary-search-account-menu-mobile";
-  const renderMobileMenu = (
+const TobBarProfileMenuMobile = ({
+  mobileMoreAnchorEl,
+  isMobileMenuOpen,
+  handleMobileMenuClose,
+  handleProfileMenuOpen,
+}) => {
+  return (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
         vertical: "top",
         horizontal: "right",
       }}
-      id={mobileMenuId}
       keepMounted
       transformOrigin={{
         vertical: "top",
@@ -120,7 +155,7 @@ export const TopBar = ({ handleMobileDrawerToggle, handleDrawerToggle }) => {
       onClose={handleMobileMenuClose}
     >
       <MenuItem>
-        <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+        <IconButton size="large" color="inherit">
           <Badge badgeContent={4} color="error">
             <MailIcon />
           </Badge>
@@ -128,11 +163,7 @@ export const TopBar = ({ handleMobileDrawerToggle, handleDrawerToggle }) => {
         <p>Messages</p>
       </MenuItem>
       <MenuItem>
-        <IconButton
-          size="large"
-          aria-label="show 17 new notifications"
-          color="inherit"
-        >
+        <IconButton size="large" color="inherit">
           <Badge badgeContent={17} color="error">
             <NotificationsIcon />
           </Badge>
@@ -140,99 +171,11 @@ export const TopBar = ({ handleMobileDrawerToggle, handleDrawerToggle }) => {
         <p>Notifications</p>
       </MenuItem>
       <MenuItem onClick={handleProfileMenuOpen}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
-          aria-haspopup="true"
-          color="inherit"
-        >
+        <IconButton size="large" color="inherit">
           <AccountCircle />
         </IconButton>
         <p>Profile</p>
       </MenuItem>
     </Menu>
-  );
-  return (
-    <>
-      <AppBar
-        position="fixed"
-        sx={{
-          zIndex: (theme) => theme.zIndex.drawer + 1,
-        }}
-      >
-        <Toolbar variant="dense">
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleMobileDrawerToggle}
-            sx={{ mr: 2, display: { xs: "block", sm: "none" } }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            edge="start"
-            onClick={handleDrawerToggle}
-            sx={{ mr: 2, display: { xs: "none", sm: "block" } }}
-          >
-            <SchoolIcon />
-          </IconButton>
-
-          <Typography variant="h6" noWrap component="div">
-            MUI
-          </Typography>
-
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              aria-label="show 4 new mails"
-              color="inherit"
-            >
-              <Badge badgeContent={4} color="error">
-                <MailIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              aria-controls={menuId}
-              aria-haspopup="true"
-              onClick={handleProfileMenuOpen}
-              color="inherit"
-            >
-              <AccountCircle />
-            </IconButton>
-          </Box>
-          <Box sx={{ display: { xs: "flex", md: "none" } }}>
-            <IconButton
-              size="large"
-              aria-label="show more"
-              aria-controls={mobileMenuId}
-              aria-haspopup="true"
-              onClick={handleMobileMenuOpen}
-              color="inherit"
-            >
-              <MoreVertIcon />
-            </IconButton>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      {renderMobileMenu}
-      {renderMenu}
-    </>
   );
 };
